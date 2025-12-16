c8a8749c525273838d36e1b9db7b2749b48c153f
Leonardo Sabou <leonardosabou22@gmail.com>
2025-12-16 22:30:16 +0200

    Added Spring Initializer

---
+++ backend/scmzalauvoleiBE/src/main/java/scmzalauvolei/scmzalauvoleiBE/ScmzalauvoleiBeApplication.java
@@ -0,0 +1,13 @@
+package scmzalauvolei.scmzalauvoleiBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmzalauvoleiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScmzalauvoleiBeApplication.class, args);
+	}
+
+}
---
+++ backend/scmzalauvoleiBE/src/test/java/scmzalauvolei/scmzalauvoleiBE/ScmzalauvoleiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package scmzalauvolei.scmzalauvoleiBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmzalauvoleiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
